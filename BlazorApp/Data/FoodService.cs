011700de59b39ff561a298333ecbe7668331acdf
kkjn0131 <kkjn0131@naver.com>
2024-09-13 22:05:50 +0900

    - Dependency Injection(의존성 주입) 실습  > service를 추가 후 사용할 razor 컴포넌트에서 inject해 사용한다.  > service의 3가지 모드의 생명주기 테스트

---
+++ BlazorApp/Data/FoodService.cs
@@ -0,0 +1,102 @@
+﻿namespace BlazorApp.Data
+{
+	public class Food
+	{
+		public string Name { get; set; }
+		public int Price { get; set; }
+	}
+
+	// 인터페이스로 개선
+	public interface IFoodService
+	{
+		IEnumerable<Food> GetFoods();
+	}
+
+	public class FoodService : IFoodService
+	{
+		public IEnumerable<Food> GetFoods()
+		{
+			List<Food> foods = new List<Food>()
+			{
+				new Food(){Name = "Bibimbap", Price = 7000 },
+				new Food(){Name = "Kimbap", Price = 3000 },
+				new Food(){Name = "Bossam", Price = 9000 }
+			};
+
+			return foods;
+		}
+	}
+
+	public class FastFoodService : IFoodService
+	{
+		public IEnumerable<Food> GetFoods()
+		{
+			List<Food> foods = new List<Food>()
+			{
+				new Food(){Name = "Hamburger", Price = 5000 },
+				new Food(){Name = "Fries", Price = 1000 }
+			};
+
+			return foods;
+		}
+	}
+
+	// Dependency Injection을 활용하면 생성자에서 서비스를 연동해준다
+	// injection한 razor컴포넌트에서 확인가능
+	public class PaymentService
+	{
+		IFoodService _service;
+
+		public PaymentService(IFoodService service)
+		{
+			_service = service;
+		}
+
+		// TODO
+	}
+
+	public class SingletonService : IDisposable
+	{
+		public Guid ID { get; set; }
+
+		public SingletonService()
+		{
+			ID = Guid.NewGuid();
+		}
+
+		public void Dispose()
+		{
+			Console.WriteLine("SingletonService Disposed");
+		}
+	}
+
+	public class TransientService : IDisposable
+	{
+		public Guid ID { get; set; }
+
+		public TransientService()
+		{
+			ID = Guid.NewGuid();
+		}
+
+		public void Dispose()
+		{
+			Console.WriteLine("TransientService Disposed");
+		}
+	}
+
+	public class ScopedService : IDisposable
+	{
+		public Guid ID { get; set; }
+
+		public ScopedService()
+		{
+			ID = Guid.NewGuid();
+		}
+
+		public void Dispose()
+		{
+			Console.WriteLine("ScopedService Disposed");
+		}
+	}
+}
--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,36 +1,55 @@
 using BlazorApp.Components;
+using BlazorApp.Data;
 
 namespace BlazorApp
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+			// Add services to the container.
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
 
-            var app = builder.Build();
+			#region Dependency Injection 실습
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Dependency Injection(의존성 주입) 테스트
+			builder.Services.AddSingleton<IFoodService, FastFoodService>();
+			// 생성자에서 알아서 연결해준다.
+			builder.Services.AddSingleton<PaymentService>();
 
-            app.UseHttpsRedirection();
+			// 3가지 모드의 생명 주기
+			// 서버가 동작하고 생성이후 변화가없음
+			// >> 데이터 변동이 없고 누구한테나 똑같은 정보를 보여야 할 때 활용가능
+			builder.Services.AddSingleton<SingletonService>();
+			// 인터페이스 요청이 있을때 마다 변화가 발생 
+			builder.Services.AddTransient<TransientService>();
+			// HTTP 요청당 한번 생성 후 재사용 (새로고침 등 HTTP 요청이 새로 들어오면 다시 생성)
+			builder.Services.AddScoped<ScopedService>();
 
-            app.UseStaticFiles();
-            app.UseAntiforgery();
+			#endregion
 
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
+			var app = builder.Build();
 
-            app.Run();
-        }
-    }
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+			app.UseAntiforgery();
+
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
+
+			app.Run();
+		}
+	}
 }
