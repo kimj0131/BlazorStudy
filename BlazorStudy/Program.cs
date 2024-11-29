911ea35f6da4ee5f0dd1060885121464f04bb3b6
kkjn0131 <kkjn0131@naver.com>
2024-11-29 12:55:51 +0900

    -  SPA (Single Page Application) 학습과 Route 방식

---
+++ BlazorStudy/Program.cs
@@ -0,0 +1,40 @@
+using BlazorStudy.Components;
+
+namespace BlazorStudy
+{
+	// SPA (Single Page Application)
+	// 하나의 틀안에서 작은 일부만 변화시키는 개념
+
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+			app.UseAntiforgery();
+
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
+
+			app.Run();
+		}
+	}
+}
