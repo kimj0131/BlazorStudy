0aa68fb25d8208d5c4b431d1ee6622b9b154b166
kkjn0131 <kkjn0131@naver.com>
2024-12-01 00:59:40 +0900

    - 상태관리(State Management) 학습

---
+++ BlazorStudy2/Data/CounterState.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorStudy2.Data
+{
+	public class CounterState
+	{
+		int _count = 0;
+
+		public Action OnStateChanged;
+
+		public int Count
+		{
+			get
+			{
+				return _count;
+			}
+			set
+			{
+				_count = value;
+				Refresh();
+			}
+		}
+
+		void Refresh()
+		{
+			OnStateChanged?.Invoke();
+		}
+
+	}
+}
